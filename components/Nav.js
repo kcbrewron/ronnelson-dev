45605995228afa473460e16b692c9bfe0f0fca8c
Kcbrewron <ronnelson929@gmail.com>
2023-02-04 16:25:20 -0600

    fixes for Link elements in nextjs components

--- components/Nav.js
+++ components/Nav.js
@@ -2,78 +2,76 @@ import Link from 'next/link';
 import Head from 'next/head';
 
 export default function Nav() {
-  return (
-    <>
-      <Head>
-        <title>Ronnelson.dev-Homepage</title>
-        <link rel='icon' href='/favicon.ico' />
-        <meta charSet='UTF-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <meta
-          itemProp='description'
-          content='My website platform for learning development, building my skills, and challenging myself.'
-        />
-        <meta
-          itemProp='image'
-          content='https://images.ctfassets.net/2tyl7ps8aucz/kMkX26GTOrfMLDfty6iMS/420f8da8e7414e49d58f330c068d8d8c/ronnelson.jpg'
-        />
+  return <>
+    <Head>
+      <title>Ronnelson.dev-Homepage</title>
+      <link rel='icon' href='/favicon.ico' />
+      <meta charSet='UTF-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta
+        itemProp='description'
+        content='My website platform for learning development, building my skills, and challenging myself.'
+      />
+      <meta
+        itemProp='image'
+        content='https://images.ctfassets.net/2tyl7ps8aucz/kMkX26GTOrfMLDfty6iMS/420f8da8e7414e49d58f330c068d8d8c/ronnelson.jpg'
+      />
 
-        <meta property='og:url' content='https://ronnelson.dev' />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content='Ronnelson.dev' />
-        <meta
-          property='og:description'
-          content='My website platform for learning development, building my skills, and challenging myself.'
-        />
-        <meta
-          property='og:image'
-          content='https://images.ctfassets.net/2tyl7ps8aucz/kMkX26GTOrfMLDfty6iMS/420f8da8e7414e49d58f330c068d8d8c/ronnelson.jpg'
-        />
+      <meta property='og:url' content='https://ronnelson.dev' />
+      <meta property='og:type' content='website' />
+      <meta property='og:title' content='Ronnelson.dev' />
+      <meta
+        property='og:description'
+        content='My website platform for learning development, building my skills, and challenging myself.'
+      />
+      <meta
+        property='og:image'
+        content='https://images.ctfassets.net/2tyl7ps8aucz/kMkX26GTOrfMLDfty6iMS/420f8da8e7414e49d58f330c068d8d8c/ronnelson.jpg'
+      />
 
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:title' content='Ronnelson.dev' />
-        <meta
-          name='twitter:description'
-          content='My website platform for learning development, building my skills, and challenging myself.'
-        />
-        <meta
-          name='twitter:image'
-          content='https://images.ctfassets.net/2tyl7ps8aucz/kMkX26GTOrfMLDfty6iMS/420f8da8e7414e49d58f330c068d8d8c/ronnelson.jpg'
-        />
-      </Head>
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:title' content='Ronnelson.dev' />
+      <meta
+        name='twitter:description'
+        content='My website platform for learning development, building my skills, and challenging myself.'
+      />
+      <meta
+        name='twitter:image'
+        content='https://images.ctfassets.net/2tyl7ps8aucz/kMkX26GTOrfMLDfty6iMS/420f8da8e7414e49d58f330c068d8d8c/ronnelson.jpg'
+      />
+    </Head>
 
-      <div className='grid grid-cols-12 grid-flow-col-dense grid-rows-1 bg-gradient-to-r from-vividSky to-vividSky pb-1'>
-        <div className='sticky top-0 z-40 py-5 bg-black flex-none flex'>
-          <div className='cols-span-2 ... text-2xl text-blueJeans pl-4 left-2'>
-            [ ronnelson.dev ]
-          </div>
-          <div className='col-start-8 ... text-xl text-blueJeans px-4 absolute right-4'>
-            <Link href='/' className='px-4 rounded-md' alt='Home'>
-              <a className='px-4 hover:bg-charcoal hover:text-ghost rounded-md'>
-                Home
-              </a>
-            </Link>
-            <Link
-              href='/about'
-              className='col-start-9 ...  justify-end rounded-md'
-              alt='About Me'
-            >
-              <a className='px-4 hover:bg-charcoal hover:text-ghost rounded-md'>
-                About
-              </a>
-            </Link>
-            <Link
-              href='/blog'
-              className='col-start-10 ... justify-end rounded-md'
-              alt='blog'
-            >
-              <a className='px-4 hover:bg-charcoal hover:text-ghost rounded-md'>
-                Blog
-              </a>
-            </Link>
-          </div>
+    <div className='grid grid-cols-12 grid-flow-col-dense grid-rows-1 bg-gradient-to-r from-vividSky to-vividSky pb-1'>
+      <div className='sticky top-0 z-40 py-5 bg-black flex-none flex'>
+        <div className='cols-span-2 ... text-2xl text-blueJeans pl-4 left-2'>
+          [ ronnelson.dev ]
+        </div>
+        <div className='col-start-8 ... text-xl text-blueJeans px-4 absolute right-4'>
+          <Link href='/' className='px-4 rounded-md' alt='Home'>
+            
+              Home
+            
+          </Link>
+          <Link
+            href='/about'
+            className='col-start-9 ...  justify-end rounded-md'
+            alt='About Me'
+          >
+            
+              About
+            
+          </Link>
+          <Link
+            href='/blog'
+            className='col-start-10 ... justify-end rounded-md'
+            alt='blog'
+          >
+            
+              Blog
+            
+          </Link>
         </div>
       </div>
-    </>
-  );
+    </div>
+  </>;
 }
--- components/Profile.js
+++ components/Profile.js
@@ -6,54 +6,50 @@ const myLoader = ({ src, width,quality }) => {
   return `${src}?w=${width}&q=${quality}`;
 };
 export default function Profile() {
-  return (
-    <>
-      <Link href='https://github.com/kcbrewron' className='space-x-1.0'>
-        <a className='space-x-2.5 hover:text-ghost'>
-          <Image
-            loader={myLoader}
-            src='/github.png'
-            placeholder='empty'
-            width={16}
-            height={16}
-            quality={100}
-            ßaria-label='github'
-            alt='github logo'
-          />
-          Github
-        </a>
-      </Link>
-      <Link href='https://www.linkedin.com/in/kc-ronnelson/'>
-        <a className='px-4 hover:text-ghost'>
-          <Image
-            loader={myLoader}
-            className='space-x-0.5'
-            placeholder='empty'
-            src='/linkedin.png'
-            width={16}
-            height={16}
-            quality={100}
-            aria-label='linkedin'
-            alt='linkedin logo'
-          />
-          LinkedIn
-        </a>
-      </Link>
-      <Link href='https://twitter.com/kcbrewron'>
-        <a className='space-x-0.5 hover:text-ghost'>
-          <Image
-            loader={myLoader}
-            placeholder='empty'
-            src='/twitter.png'
-            width={16}
-            height={16}
-            quality={100}
-            aria-label='twitter'
-            alt='twitter logo'
-          />
-          Twitter
-        </a>
-      </Link>
-    </>
-  );
+  return <>
+    <Link href='https://github.com/kcbrewron' className='space-x-1.0'>
+
+      <Image
+        loader={myLoader}
+        src='/github.png'
+        placeholder='empty'
+        width={16}
+        height={16}
+        quality={100}
+        ßaria-label='github'
+        alt='github logo'
+      />Github
+    </Link>
+    <Link
+      href='https://www.linkedin.com/in/kc-ronnelson/'
+      className='px-4 hover:text-ghost'>
+
+      <Image
+        loader={myLoader}
+        className='space-x-0.5'
+        placeholder='empty'
+        src='/linkedin.png'
+        width={16}
+        height={16}
+        quality={100}
+        aria-label='linkedin'
+        alt='linkedin logo'
+      />LinkedIn
+    </Link>
+    <Link
+      href='https://twitter.com/kcbrewron'
+      className='space-x-0.5 hover:text-ghost'>
+
+      <Image
+        loader={myLoader}
+        placeholder='empty'
+        src='/twitter.png'
+        width={16}
+        height={16}
+        quality={100}
+        aria-label='twitter'
+        alt='twitter logo'
+      />Twitter
+    </Link>
+  </>;
 }
