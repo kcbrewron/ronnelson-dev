a256882ca5ca72537b6eb091a104ccfaadbd13be
Kcbrewron <kcbrewron@users.noreply.github.com>
2023-02-24 21:01:51 -0600

    Update loader.js

--- components/loader.js
+++ components/loader.js
@@ -1,7 +1,7 @@
 export default function myImageLoader({ src, width, height, quality }) {
     console.log("image source supplied " +src)
     console.log("width " +width + " quality =>" +quality)
-    const imgloaderUrl =`${src}?fm=jpg&fl=progressive`
+    const imgloaderUrl =`${src}`
     console.log("Image Loader Value "+imgloaderUrl);
   return imgloaderUrl;
 }
