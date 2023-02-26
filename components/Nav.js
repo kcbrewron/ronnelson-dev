62e0469f8fe4dcee9bd74a294af8b91ccf7dbf0c
Kcbrewron <ronnelson929@gmail.com>
2023-02-25 20:03:52 -0600

    image quality improvements and accessibility issues

--- components/Nav.js
+++ components/Nav.js
@@ -111,7 +111,7 @@ export default function Nav() {
 
           {/* Mobile responsive menu here */}
           <div class="md:hidden flex items-center text-indigoBlue">
-              <button className="mobile-menu-button px-2" onClick={openMenu}>
+              <button className="mobile-menu-button px-2" role={menu} aria-label="menu" onClick={openMenu}>
                 <Bars3Icon className={!menuState ? "visible h-6 w-6": "hidden"} />
                 <XMarkIcon className={menuState ? "visible h-6 w-6": "hidden"} />
               </button>
--- utils/loader.js
+++ utils/loader.js
@@ -1,7 +1,7 @@
 export default function myImageLoader({ src, width, height, quality }) {
     console.log("image source supplied " +src)
     console.log("width " +width + " quality =>" +quality)
-    const imgloaderUrl =`${src}?w=${width}&q=${quality}&fm=jpg&fl=progressive`
+    const imgloaderUrl =`${src}?w=${width}&q=${quality}&fm=webp`
     console.log("Image Loader Value "+imgloaderUrl);
   return imgloaderUrl;
 }
