4dc877323dda0a04c863861dbac559000155abef
Kcbrewron <ronnelson929@gmail.com>
2023-03-05 17:33:15 -0600

    fix for metadata retrieval

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,13 +1,32 @@
 import { fetchLandingPage } from "../../../utils/api";
 import LandingPagePost from "../../../components/LandingPage";
 import { notFound } from "next/navigation";
-//import type { Metadata } from "next";
+import type { Metadata } from "next";
 
 async function getData(slug: string) {
   const pageContent = await fetchLandingPage(slug["0"]).catch((err) => {
     console.log("Error retrieving content " + err);
   });
   return pageContent;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  console.log("Get metadata for " + params);
+  const response = await getData(params.slug);
+  const metaData = response.seoMetadata ? response.seoMetadata : {};
+  console.log(metaData);
+  const title = metaData.seoTitle || "";
+  const description = metaData.seoDescription || "";
+  const keywords = metaData.seoKeywords || "";
+  return {
+    title: title,
+    description: description,
+    keywords: keywords,
+  };
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
